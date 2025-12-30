a21ff9911b004a06109e29ff7e59491f658700ce
saskiacampbell <scampbell@thetrustees.org>
2025-12-30 11:00:38 -0500

    question 3c

--- harvest.R
+++ harvest.R
@@ -1,4 +1,5 @@
-#this script is to answer QUESTION 3C
+#this script is to answer QUESTION 3C for 2025 grazing planning
+#total forage harvested per field for grazing and haying events
 
 library(tidyverse)
 library(dplyr)
@@ -8,7 +9,7 @@ actuals_platemeter_final <-read.csv ("actuals_platemeter_final")
 #calculating total harvested for grazes
 cover_change_df <- actuals_platemeter_final |>
   filter(move_type %in% c("day_in", "day_out")) |>
-  select(field, harvest_cycle, move_type, cover, acres) |>
+  select(field, harvest_cycle, move_type, cover, acres, acres_impacted) |>
   pivot_wider(
     names_from = move_type,
     values_from = cover,
@@ -23,32 +24,102 @@ cover_change_df <- actuals_platemeter_final |>
 
 #take average harvests per field
 avg_harvest <- cover_change_df |>
-  group_by(field, acres) |>
+  group_by(field, acres, acres_impacted) |>
   summarize(
     avg_cover_change = mean(cover_change, na.rm = TRUE),
     .groups = "drop"
   )
 
-#calculating lbs_per_acre
-#how to use acres impacted to have an accurate lbs_per_acre???
-avg_harvest_acres <- avg_harvest |>
-  mutate(
-    lbs_per_acre = avg_cover_change / acres
+#calculating avgerage acres impacted by field
+avg_harvest <- avg_harvest |>
+  group_by(field) |>
+  summarize(
+    avg_acres = mean(acres_impacted, na.rm = TRUE),
+    avg_cover_change = mean(avg_cover_change, na.rm = TRUE),
+    .groups = "drop"
   )
 
 
-#plot average
 
+#plot average
 ggplot(
-    avg_harvest_acres |> 
-      filter(!is.na(lbs_per_acre)), 
+    avg_harvest |> 
+      filter(!is.na(avg_cover_change)), 
     aes(
-      x = reorder(field, lbs_per_acre), 
-      y = lbs_per_acre,
+      x = reorder(field, avg_cover_change), 
+      y = avg_cover_change,
     )
   ) +
     geom_point()+
     theme(
       axis.text.x = element_text(angle = 90, hjust = 1, size = 6)
-    ) 
+    ) +
+  labs(
+    x= "Field",
+    y="lbs per acres",
+    title = "Forage Harvested by Grazes"
+  )
+
+
+#####################
+#now look at hay harvested
+hay_change <- actuals_platemeter_final |>
+  filter(move_type %in% c("hay_in", "hay_out")) |>
+  select(field, harvest_cycle, move_type, cover, acres, acres_impacted) |>
+  pivot_wider(
+    names_from = move_type,
+    values_from = cover,
+    values_fn = mean  
+  ) |>
+  mutate(
+    day_in  = as.numeric(hay_in),
+    day_out = as.numeric(hay_out),
+    cover_change = day_in - day_out
+  )
+
+#average hay harvest
+avg_hay_harvest <- hay_change |>
+  group_by(field, acres_impacted) |>
+  summarize(
+    avg_cover_change = mean(cover_change, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+#calculating avgerage acres impacted by field
+avg_hay_harvest <- avg_hay_harvest |>
+  group_by(field) |>
+  summarize(
+    avg_acres = mean(acres_impacted, na.rm = TRUE),
+    avg_cover_change = mean(avg_cover_change, na.rm = TRUE),
+    .groups = "drop"
+  )
+
+#plotting
+ggplot(
+  avg_hay_harvest |> 
+    filter(!is.na(avg_cover_change)), 
+  aes(
+    x = reorder(field, avg_cover_change), 
+    y = avg_cover_change,
+  )
+) +
+  geom_point()+
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1, size = 6)
+  ) +
+  labs(
+    x= "Field",
+    y="lbs per acres",
+    title = "Forage Harvested by Hays"
+  )
+
+
+
+
+
+
+
+
+
+
   
