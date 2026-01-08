297b54d656036cc42476f00af709c5903259f196
saskiacampbell <scampbell@thetrustees.org>
2026-01-08 14:28:51 -0500

    graphs

--- field_cyles.R
+++ field_cyles.R
@@ -9,32 +9,6 @@ library(ggpattern)
 #reading in script
 actuals_platemeter_final<-read.csv("actuals_platemeter_final")
 
-#need to assign herds to a few wonky data binds
-actuals_platemeter_final <- actuals_platemeter_final %>%
-  mutate(herd = if_else(
-    field %in% c(
-      "Underhill Wet Meadow",
-      "Williams West"
-    ) &
-      is.na(herd),
-    "feeders",
-    herd
-  ))
-
-actuals_platemeter_final <- actuals_platemeter_final %>%
-  mutate(herd = if_else(
-    field %in% c(
-      "GP1A",
-      "GP6A",
-      "Leo's"
-    ) &
-      is.na(herd),
-    "brood",
-    herd
-  ))
-
-
-
 
 #counting harvest cycles according to <50% acreage exclusion rule
 harvest_cycles_by_field <- actuals_platemeter_final |>
@@ -47,8 +21,10 @@ harvest_cycles_by_field <- actuals_platemeter_final |>
 
 
 #plot harvest cycles by field, coloring bars by herd
+
 ggplot(
-  harvest_cycles_by_field,
+  harvest_cycles_by_field %>%
+    filter(!is.na(herd)),
   aes(
     x = fct_reorder(field, total_harvest_cycles, .fun = sum),
     y = total_harvest_cycles,
@@ -59,9 +35,9 @@ ggplot(
   scale_y_continuous(expand = c(0, 0), limits = c(0, NA)) +
   theme(
     axis.text.x = element_text(
-      angle = 90,   
-      hjust = 1,      
-      vjust = 0.5,    
+      angle = 90,
+      hjust = 1,
+      vjust = 0.5,
       size = 7
     )
   ) +
@@ -70,5 +46,3 @@ ggplot(
     y = "Total Harvest Cycles",
     title = "Harvest Cycles by Field"
   )
-
-
